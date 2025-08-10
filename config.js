3360dfc8cad6dfbe3d73542d1bbfb5ea8d5d3c10
sanija151 <brosanija388@gmail.com>
2025-08-10 09:42:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'SANIJA-MD=zQkWzS4J#o1NJ1JzlJhH7zy8qY0ld4uNHE4c1EjrSiJOadQ8LjL4' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'SANIJA-MD=PZ80FZSI#4R29ULuVeIDExCO-dwbnekbsGkq4cE4wzgxVy9yW16I' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'sanijamd' : process.env.SESSION_NAME, 
 
